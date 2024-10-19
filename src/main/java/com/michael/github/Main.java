6346139204d1ff80db055d8248b0967020a41d7d
MichaelKotwica <45274996+MichaelKotwica@users.noreply.github.com>
2024-10-19 15:41:31 -0500

    added logging files

--- src/main/java/com/michael/github/Main.java
+++ src/main/java/com/michael/github/Main.java
@@ -1,7 +1,18 @@
 package com.michael.github;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        logger.info("info");
+        logger.error("Error");
+        logger.debug("debug");
+        logger.fatal("fatal");
+
     }
 }
