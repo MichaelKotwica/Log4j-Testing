5f6efcea29e93570419dad01dc59c38a9df3ffa9
MichaelKotwica <45274996+MichaelKotwica@users.noreply.github.com>
2024-10-20 09:48:20 -0500

    small code cleanup

--- src/main/java/com/michael/github/Main.java
+++ src/main/java/com/michael/github/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        newClass = new newClass(7) ;
+        newClass = new newClass(7);
 
         System.out.println("Hello world!");
         logger.info("info");
